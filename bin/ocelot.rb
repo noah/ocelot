31a3f219d0ba2028af47851e7b507ffe90996d5d
Noah Tilton <noaht@wolfram.com>
2012-08-14 12:00:25 -0500

    packaging

---
+++ bin/ocelot.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env jruby
+#
+# = About OCeLoT (ODF Toolkit Command Line Tools)
+#
+# This program provides a DSL for creating and manipulating Open
+# Document files.
+#
+# Author::      Noah K. Tilton
+# Copyright::   Copyright (c) 2012 Noah K. Tilton
+# License::     Apache 2.0
+
+# Prepend the lib directory one level up to the library search path
+$:.unshift File.expand_path(File.join(File.dirname(__FILE__), '../lib'))
+
+Dir["./lib/jars/*.jar"].each do |jar|
+  require jar
+end
+
+require "java"
+
+# N.B.: '$' delimiter means java ENUM: http://jira.codehaus.org/browse/JRUBY-6092
+%w[ 
+  java.net.URI
+  org.odftoolkit.simple.PresentationDocument
+  org.odftoolkit.simple.Component
+  org.odftoolkit.simple.table.Table
+  org.odftoolkit.simple.TextDocument
+  org.odftoolkit.simple.TextDocument$OdfMediaType
+  org.odftoolkit.simple.style.StyleTypeDefinitions$HorizontalAlignmentType
+  org.odftoolkit.simple.style.StyleTypeDefinitions$FontStyle
+  org.odftoolkit.simple.common.TextExtractor
+  org.odftoolkit.simple.common.EditableTextExtractor
+  org.odftoolkit.simple.presentation.Slide$SlideLayout
+].each {|klass| java_import klass}
+
+require "ocelot"
+
+# Program usage
+def usage
+  puts "java -jar #{$0} <input file>"
+  exit -1
+end
+
+# Evaluate the input file
+ARGV[0].nil ? usage : require ARGV[0]
