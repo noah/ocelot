8ef676a9a4e9f0cd60ac59fb6416878a6bad89e5
Noah Tilton <noaht@wolfram.com>
2012-08-17 15:26:41 -0500

    array overload methods

--- lib/table.rb
+++ lib/table.rb
@@ -1,2 +1,25 @@
 module OCELOT
+
+  Java::OrgOdftoolkitSimpleTable::Cell.class_eval do
+
+    def << val
+      self.set_string_value val
+    end
+
+  end
+
+  Java::OrgOdftoolkitSimpleTable::Table.class_eval do
+
+    # Read table cell using array syntax
+    def [] x, y
+      self.get_cell_by_position x, y
+    end
+
+    # Set table cell using array syntax
+    def []= x, y, value
+      (self.get_cell_by_position x, y).set_string_value value
+    end
+
+  end
+
 end
